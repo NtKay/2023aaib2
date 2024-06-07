aca49156426ed15839f541c4b4bdd4f61e4f788b
ntkay <145088363+ntkay@users.noreply.github.com>
2024-06-07 11:43:23 +0800

    week16
    week16

---
+++ week16/week16-5c2.cpp
@@ -0,0 +1,15 @@
+#include <string.h>
+int main(){
+	int a[7] = {};
+	char s[200];
+	scanf("%s",s);
+	int N = strlen(s);
+	for(int i=0;i<N;i++){
+		for(int d=1; d<=6; d++){
+			if(s[i]=='0'+d) a[d]++;
+		}
+	}
+	for(int d=1; d<=6; d++){
+		printf("%d:%d\n",d,a[d]);
+	}
+}
